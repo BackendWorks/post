b768797fb5c9958b7d55063a772463a059ba74c0
Harsh Makwana <harsh.make1998@gmail.com>
2022-10-27 11:14:02 +0530

    fix: test applied

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,40 @@
-import { Body, Controller, Get, Param, Post, Put, Query } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpStatus,
+  Param,
+  Post,
+  Put,
+  Query,
+  Res,
+} from '@nestjs/common';
 import { AppService } from './app.service';
+import { Response } from 'express';
 import { CreatePostDto, GetPostsDto, UpdatePostDto } from './core/dtos';
 import { CurrentUser } from './core/user.decorator';
+import { AllowUnauthorizedRequest } from './core/allow.unauthorized.decorator';
+import { PrismaService } from './core/services/prisma.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private prisma: PrismaService,
+  ) {}
+
+  @AllowUnauthorizedRequest()
+  @Get('/health')
+  public healthCheck(@Res() res: Response) {
+    this.prisma
+      .$connect()
+      .then(() => {
+        return res.status(HttpStatus.OK).json({ stauts: 'ok' });
+      })
+      .catch((e) => {
+        return res.status(HttpStatus.OK).json({ stauts: 'down', error: e });
+      });
+  }
 
   @Post('/create')
   createPost(@Body() data: CreatePostDto, @CurrentUser() authUserId: number) {
--- src/core/guards/auth.guard.ts
+++ src/core/guards/auth.guard.ts
@@ -4,6 +4,7 @@ import {
   Inject,
   Injectable,
 } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
 import { ClientProxy } from '@nestjs/microservices';
 import { firstValueFrom } from 'rxjs';
 
@@ -11,6 +12,7 @@ import { firstValueFrom } from 'rxjs';
 export class ClientAuthGuard implements CanActivate {
   public constructor(
     @Inject('AUTH_SERVICE') private readonly client: ClientProxy,
+    private reflector: Reflector,
   ) {
     this.client.connect();
   }
@@ -18,6 +20,13 @@ export class ClientAuthGuard implements CanActivate {
   public async canActivate(context: ExecutionContext): Promise<boolean> {
     try {
       const request = context.getArgByIndex(0);
+      const allowUnauthorizedRequest = this.reflector.get<boolean>(
+        'allowUnauthorizedRequest',
+        context.getHandler(),
+      );
+      if (allowUnauthorizedRequest) {
+        return true;
+      }
       const Authorization = request.headers['authorization'];
       const token = Authorization.replace('Bearer ', '');
       const decode = await firstValueFrom(
