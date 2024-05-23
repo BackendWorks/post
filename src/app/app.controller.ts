32877be483207bd72bc22dece1ebc869c21e6cf1
Harsh Makwana <harsh.make1998@gmail.com>
2024-05-23 16:16:36 +0530

    fix: health controller version route

--- src/app/app.controller.ts
+++ src/app/app.controller.ts
@@ -1,9 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, VERSION_NEUTRAL } from '@nestjs/common';
 import { Public } from '../decorators/public.decorator';
 import { HealthCheck, HealthCheckService } from '@nestjs/terminus';
 import { PrismaService } from '../common/services/prisma.service';
 
-@Controller()
+@Controller({
+  version: VERSION_NEUTRAL,
+  path: '/',
+})
 export class AppController {
   constructor(
     private healthCheckService: HealthCheckService,
